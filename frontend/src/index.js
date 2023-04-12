52eab3df2d1430e178dd95a1f4592fc028d739d0
Srushhhti <ssrushti108@gmail.com>
2023-04-12 10:35:57 +0530

    nothing just deleted

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import FrontSignup from './FrontSignup'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-<div style={{color:"hello",backgroundColor:"blue"}} class="hello"></div>
-    <App />
     <FrontSignup/>
   </React.StrictMode>
 );
